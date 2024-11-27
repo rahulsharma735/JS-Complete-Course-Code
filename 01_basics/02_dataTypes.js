7a6c9c65b7213afe944550524c5e370e4b16c669
Rahul Sharma <118681201+rahulsharma735@users.noreply.github.com>
2024-11-27 05:57:08 +0000

    CONATINER CONFIG DONE

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,34 @@
+//COMMAND FOR THE RUNNIG THE FILE IN THE NODE "node 01_basics/02_dataTypes.js".
+
+"use strick"; //treat all JS code as newer vesion
+
+//alert("hello"); //We are usng Noe js, not browser
+
+//console.log(3+3) console.log("rahul") // Give the error during the output or this is the not goof practise for writting the code.
+console.log(3+3); console.log("rahul") //Code readablity should be high.
+
+// Documentaion for the javaScrypt original should be follow as "https://tc39.es/ecma262/"
+// Above documenttion is used for the engine or broswer likhne ke liye hai ye bass
+
+let name = "rahul sharma"
+let age = 21;
+let isLoggedIn = false;
+let state = null;
+
+//number => 2 to power 53.
+// bigint
+// string => " "
+
+//boolean => true/false
+// null -> stand alone value in javaScrypt(type)("representaion of the empty value")
+
+// undefined => (undefined tells about that variable was decleared but not initialize.)assingend nahi hui hai
+// symbol  => unique (it is used for the uniqueness )
+// object
+
+console.log(typeof "rahul")
+console.log(typeof 10)
+console.log(typeof state)//show about the object type.
+console.log(typeof undefined)
+
+// ASSIGNMENT :- READ SOME DOCUMENTAION ON THE ECMA FOR THE DATATYPES. 
