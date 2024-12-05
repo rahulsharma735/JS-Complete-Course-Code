f11f57c164ffa36891a17239c0f7d2824e4dc4d2
Rahul Sharma <118681201+rahulsharma735@users.noreply.github.com>
2024-12-05 16:12:53 +0000

    container config done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -32,3 +32,52 @@ let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
 console.log(typeof stringNumber);
+
+/*  ********************************* Operation  ********************************* */
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+// console.log("AIRTHMATIC OPERATOR IN JAVASCRYPT")
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/2)
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = " rahul"
+
+let str3 = str1+ str2
+console.log(str3)
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+"2");
+// console.log("1"+ 2 + 2);
+// console.log(1+ 2 + "2");
+
+// console.log( (3+4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+ "");
+
+let num1 , num2, num3
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+
+
+
+
+
+
+
+
+//01_basics/03_conversionOperation.js
