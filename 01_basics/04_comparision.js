7d01b9b4f2bc585ffda352a021e7bfd6bf372fc7
Rahul Sharma <118681201+rahulsharma735@users.noreply.github.com>
2024-12-05 17:54:35 +0000

    container config done

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,26 @@
+// console.log(2 > 1);
+// console.log( 2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+ 
+// IN JAVA SCRYPT BOTH THE OPERATOR "==" && ">=" ARE DIFFERENT AND THIER WORKING IS ALSO DIFFERENT.
+// is type ke conversion apne ko kabhi bhi confusion me dal sakte hai so it is not the best pracice for the compariosn in javascipt.
+
+// console.log(null == 0);
+// console.log(null > 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+
+// "===" IS IS CALLED AS STRICKED CHECK -> YE APKI 'VALUE' KO NAHI USKE 'DATATYPE' KO BHI CHEK KRTA HAI DURING COMPARISON.
+console.log("2" == 2);
+console.log("2" === 2);
+
---
+++ datatpes-summary.js
@@ -0,0 +1,94 @@
+/*Javascript has dynamically typed language and from one point of view, it is good because 
+it’s pretty simple to assign values to the variables. */
+
+// Primitive (call by value = copy version)
+/*  7 types  : string , Number, Boolean, null(empty), undefinded, Symbol(it is used to make something unique), BigInt*/
+
+const score = 100;
+const scoreValue = 100.3
+
+const isLoggedIN = true;
+const outSideTemp = null;//
+
+let userEmail;
+
+const id = Symbol('123')
+const id1  =  Symbol('123')
+
+// console.log(id == id1);// Id of both the ids would be always unique.
+
+// const bigNumber = 54754153215821548n 
+
+
+
+/*Non Primitive (Reference type) {call by referene if once  we change the value at that time automatically change in the 
+ value at all reference .}*/
+
+/* Arrays, Objects(imp), Function  */
+
+const heros = ["rahul", "neeraj", "kamal"] // ARRAYS
+
+let myObj = {
+    name: "rahul",
+    age : 222
+}   // It is the object in javascript.
+
+const myfunction = function(){
+    console.log("hello world");
+}
+
+console.log(typeof bigNumber);
+console.log(typeof outSideTemp);
+console.log(typeof scoreValue);
+
+console.log(typeof id);
+
+
+console.log(typeof myfunction);//called as object Function (function object)
+
+
+/*
+* Premitive Datatypes
+
+                Type                                typeof
+
+i)             Number                               number
+ii)            String                               string
+iii)           Boolean                              boolean
+iv)           Bigint                                bigint
+v)            Symbol                                symbol
+vi)           Null                                  object
+vii)          Undefined                             undefined
+
+
+* Non-Premitive OR Referance OR Object datatype
+
+                  Type                                   typeof
+
+i)               Object                                object
+ii)              Array                                  object
+iii)             Function                            function(object)
+*/
+
+/*
+JavaScript is a dynamically typed language. This means that variable types are determined at runtime, 
+and you do not need to explicitly declare the type of a variable before using it. You can assign different 
+types of values to a variable during its lifetime.
+
+For example, in JavaScript, you can do the following:
+
+let x = 10; // x is now a number
+x = "Hello"; // x is now a string
+x = true; // x is now a boolean
+On the other hand, statically typed languages require you to declare the variable's type explicitly, 
+and the type checking is done at compile-time, before the code is executed.
+
+Languages like Java, C++, and TypeScript are statically typed, and they require you to specify the 
+variable type explicitly when declaring them:
+
+int x = 10; // x is a variable of type int
+String name = "John"; // name is a variable of type String
+JavaScript's dynamic typing allows for more flexibility but can lead to potential runtime errors 
+if not handled carefully. Static typing, on the other hand, provides better type safety at the 
+cost of some initial verbosity and strictness.
+*/
