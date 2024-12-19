f9f0648bf34ea58b0feb82f6c25cee004c4a0a8a
Rahul Sharma <118681201+rahulsharma735@users.noreply.github.com>
2024-12-19 13:31:10 +0000

    Math & Num

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -25,4 +25,12 @@ console.log(Math.ceil(4.2));//ALWAYS TAKE UPPER
 console.log(Math.floor(4.6));//ALWAYS TAKE LOER VALUE
 
 
+console.log(Math.random());
+// console.log(Math.round(Math.random()*10));
+console.log(Math.floor(Math.random()*10) +1);
+console.log(Math.random()*10 +1);
 
+const min = 10;
+const max = 30;
+
+console.log(Math.floor(Math.random() * (max - min + 1))+min)
